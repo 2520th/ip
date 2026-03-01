2bfef41a483022abe9cb43eba5eddb6a2904dd72
2520th <jeffzgy@163.com>
2026-03-02 02:37:35 +0800

    Set up a correct Launcher Class

---
+++ src/main/java/ip/Launcher.java
@@ -0,0 +1,10 @@
+package ip;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Replace 'Main' with your actual class that extends Application
+        Application.launch(Main.class, args);
+    }
+}
